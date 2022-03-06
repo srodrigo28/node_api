68f7b061e3fc2db9a1857eaff3571abb833584f9
srodrigo28 <rodrigoexer1@gmail.com>
2022-03-06 14:16:34 -0300

    iniciando com MidWare

--- src/index.js
+++ src/index.js
@@ -40,8 +40,9 @@ app.post("/account", (req, res) => {
         return res.status(201).send();
     });
 
-    app.get("/account/:cpf", (req, res) => {
-        const { cpf } = req.params;
+    app.get("/account", (req, res) => {
+        //const { cpf } = req.params;
+        const { cpf } = req.headers;
 
         const customer = customers.find((customer) => customer.cpf === cpf );
 
